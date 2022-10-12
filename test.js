c37a1037dc531681477b29761b635177703c61bc
ncsing07 <ncsing@bamako888.com>
2022-10-12 13:56:25 +0800

    Update testjs

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 const { spec } = require('pactum');
 
 // const baseUrl = 'https://api-op-stage.thomasmov.xyz';
-const baseUrl = 'https://stageapi.thomasmov.xyz';
+const baseUrl = 'http://localhost:8012/';
 
 it('should get a response with status code 200', async () => {
   const res1 = await spec()
-    .get(baseUrl + '/site/setup')
+    .get(baseUrl + '/site/index')
     // .withJson({
     //     "LoginForm": {
     //         "username": "keladmin2",
