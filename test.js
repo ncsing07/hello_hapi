24eb9b35bd5e177c02b9db293d88a0e49ba71a28
ncsing07 <ncsing@bamako888.com>
2022-10-12 11:53:15 +0800

    Update testjs

--- test.js
+++ test.js
@@ -1,27 +1,27 @@
 const { spec } = require('pactum');
 
-const baseUrl = 'https://api-op-stage.thomasmov.xyz';
-// const baseUrl = 'https://stageapi.thomasmov.xyz';
+// const baseUrl = 'https://api-op-stage.thomasmov.xyz';
+const baseUrl = 'https://stageapi.thomasmov.xyz';
 
 it('should get a response with status code 200', async () => {
   const res1 = await spec()
-    .post(baseUrl + '/site/login')
-    .withJson({
-        "LoginForm": {
-            "username": "keladmin2",
-            "password": "keladmin",
-            "rememberMe": 1
-        }
-    })
+    .get(baseUrl + '/site/setup')
+    // .withJson({
+    //     "LoginForm": {
+    //         "username": "keladmin2",
+    //         "password": "keladmin",
+    //         "rememberMe": 1
+    //     }
+    // })
     .inspect()
     .expectStatus(200);
 
-    const token = res1.json.data.token;
+    // const token = res1.json.data.token;
 
-    await spec()
-        .get(baseUrl + '/can-shu/operator/list')
-        .withHeaders('Authorization', 'Bearer ' + token)
-        .inspect()
-        .expectStatus(200);
+    // await spec()
+    //     .get(baseUrl + '/can-shu/operator/list')
+    //     .withHeaders('Authorization', 'Bearer ' + token)
+    //     // .inspect()
+    //     .expectStatus(200);
 
 });
