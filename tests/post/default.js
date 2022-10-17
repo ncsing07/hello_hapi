38961cdb5b4a68f53cdf055bec5b72fa9878b6a5
ncsing07 <ncsing@bamako888.com>
2022-10-17 17:16:13 +0800

    Update test

---
+++ tests/post/default.js
@@ -0,0 +1,9 @@
+const { spec } = require('pactum');
+
+it('should get random users', async () => {
+  await spec()
+    .get('https://randomuser.me/api');
+});
+
+
+// var { token } = require('../site');
--- tests/site.js
+++ tests/site.js
@@ -1,41 +1,122 @@
-const { spec, request } = require('pactum');
+const { spec, request, e2e } = require('pactum');
 const { environmentData } = require('./config/env');
 
 before(() => {
     request.setBaseUrl(environmentData.baseUrl);
 });
 
+let token = '';
+
 it('should get a response with status code 200', async () => {
 
-    // let username = (Math.random() + 1).toString(36).substring(4);
-
-    // const signup = await spec()
-    //     .get(baseUrl + '/site/signup')
-    //     .withJson({
-    //         'username': username,
-    //         'password': username,
-    //         'email': username + '@test.com'
-    // })
-    // .inspect()
-    // .expectStatus(200);
+    let random = (Math.random() + 1).toString(36).substring(3);
 
     const login = await spec()
         .post('/site/login')
         .withJson({
             'username': environmentData.admin,
             'password': environmentData.admin,
-    })
-    .expectStatus(200);
+        })
+        .expectStatus(200);
 
-    const token = login.json.data.token;
+    token = login.json.data.token;
+
+    const post = await spec()
+        .post('/site/create-post')
+        .withHeaders('Authorization', 'Bearer ' + token)
+        .withJson({
+            'title': random,
+            'body': 'Here is ' + random + ' body',
+        })
+        .expectStatus(200);
 
     await spec()
-        .get('/site/view')
+        .get('/site/view-post?id={id}')
         .withHeaders('Authorization', 'Bearer ' + token)
-        .expectStatus(200);
+        .withPathParams('id', post.json.data.id)
+        .expectStatus(200)
+        .expectJson({
+            'status': 200,
+            'message': 'Data Found',
+            'data': {
+                'body': post.json.data.body,
+                'id': post.json.data.id,
+                'title': post.json.data.title
+            }
+        });
 });
 
 
-// require('./post/default');
+
+// let token = '';
+
+// it('should get a response with status code 200', async () => {
+//     const response = await spec()
+//         .post('/site/login')
+//         .withJson({
+//             'username': environmentData.admin,
+//             'password': environmentData.admin,
+//         })
+//         .inspect()
+//         .expectStatus(200);
+  
+//     token = response.json.data.token;
+  
+//     await spec()
+//         .get('/site/index')
+//         .withHeaders('Authorization', 'Bearer ' + token)
+//         .inspect()
+//         .expectStatus(200);
+// });
+
+// before(async () => {
+//     request.setBaseUrl(environmentData.baseUrl);
+//     // const response = await spec().post('/site/login').withJson({
+//     //     'username': environmentData.admin,
+//     //     'password': environmentData.admin,
+//     // }).expectStatus(200);
+//     // token = response.json.data.token;
+// });
+
+// it('should get a response with status code 200', async () => {
+
+//     // let username = (Math.random() + 1).toString(36).substring(4);
+
+//     // const signup = await spec()
+//     //     .get(baseUrl + '/site/signup')
+//     //     .withJson({
+//     //         'username': username,
+//     //         'password': username,
+//     //         'email': username + '@test.com'
+//     // })
+//     // .inspect()
+//     // .expectStatus(200);
+
+//     const login = await spec()
+//         .post('/site/login')
+//         .withJson({
+//             'username': environmentData.admin,
+//             'password': environmentData.admin,
+//     })
+//     .inspect()
+//     .expectStatus(200);
+
+//     token = login.json.data.token;
+
+//     // console.log(token);
+
+//     // module.exports = { token : token };
+
+
+//     // var post = require('./post/default');
+//     // console.log(post);
+
+//     await spec()
+//         .get('/site/index')
+//         .withHeaders('Authorization', 'Bearer ' + token)
+//         .expectStatus(200);
+// });
+
+
 
 
